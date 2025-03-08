8ae77a6db40868f0bec8351dc36b17d36fd85530
Beren8615 <berenbabaoglan702@gmail.com>
2025-03-08 21:56:02 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teknoloji Bağımlılığı İle Mücadele</title>
+    <link rel="stylesheet" href="style.css">  <!-- css dosyası bağlantısı -->
+</head>
+<body>
+    <h1>Dijital Dünyada Kaybolma, Gerçek Hayata Dön!</h1>
+    <p>Her gün milyarlarca teknolojik alet üretiliyor. Ve her gün milyonlarca insan teknoloji bağımlısı oluyor.</p>
+    <p>Dünya'da 4.5 milyar insan akıllı telefon kullanıyor. Ve 4.5 milyar insanın da %56.4'ü teknoloji bağımlısı. </p>
+    <h2>Peki Nasıl Engellenir</h2>
+    <p>Bunun için öncelikle teknoloji için harcadığın vakti kısıtla!</p>
+    <p>Teknoloji Bağımlılığını Bırakmak İçin Uygula:</p>
+             <ol>
+                   <li>Teknoloji kullanımını azalt</li>
+                   <li>Dış durdurucular kullan</li>
+                   <li>Teknoloji kullanımıyla ilgili hedefler belirle</li>
+                   <li>Çok kullanılan belli işlevlerden uzak dur</li>
+                   <li>Hatırlatıcı kartlar kullan</li>
+                   <li>Kişisel defter oluştur</li>
+                   <li>Aile terapisine başvur</li>
+                   <li>Farklı hobiler edin</li>
+             </ol>
+     <img src="https://psikologenesdincer.com.tr/wp-content/webp-express/webp-images/uploads/2023/05/Teknoloji-bagimliligi-nedir-1280x852.jpg.webp" alt="Image1">
+</body>
+</html>
